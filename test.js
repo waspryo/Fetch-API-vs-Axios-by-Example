f4e4c0383ed2380269fb4bf7156b6c3b841dfc28
waspryo <wasp2123321@gmail.com>
2020-03-15 18:21:40 +0900

    fethc api

--- test.js
+++ test.js
@@ -1,19 +1,28 @@
-// Promise
+// // Promise
 
-const getCharacterData = () => {
-    return new Promise(function(resolve, reject) {
-        const error = false;
+// const getCharacterData = () => {
+//     return new Promise(function(resolve, reject) {
+//         const error = false;
 
-        if (!error) {
-            setTimeout(() => {
-                resolve({ id: 1, name: 'Rick Wick' });
-            }, 1000);
-        } else {
-            reject('Error getting data')
-        }
+//         if (!error) {
+//             setTimeout(() => {
+//                 resolve({ id: 1, name: 'Rick Wick' });
+//             }, 1000);
+//         } else {
+//             reject('Error getting data')
+//         }
+//     })
+// }
+
+// getCharacterData()
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
+
+const url = 'https://rickandmortyapi.com/api/character/1'
+
+fetch(url)
+    .then((res) => {
+        return res.json();
+    }).then((data) => {
+        console.log('data', data);
     })
-}
-
-getCharacterData()
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
